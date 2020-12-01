86d2e25e8d73ebf0d399d2e71f4d657cebbec2e0
Kaspar Klemm <kaspar@klemmfamily.com>
2020-12-01 13:48:35 +0100

    Heroku commits

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='Apollo_20',
       version="1.0",
-      description="Project Description",
+      description="Apollo_20 App",
+      install_requires = requirements
       packages=find_packages(),
       test_suite = 'tests',
       # include_package_data: to install data from MANIFEST.in
---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"${kaspar@klemmfamily.com}\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
